0ef3e10b74e816922581e7a8d9501fbb547bd7a3
Daniel Papp <papp.daniel.gabor@gmail.com>
2022-08-03 19:19:16 +0200

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const sorted = people.sort((a, b) => {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear();
+        };
+
+        if (!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear();
+        };
+
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return nextPerson - lastPerson;
+    });
+    const oldest = sorted[0];
+    return oldest;
 
 };
 
